aae2d37282e15781cc3159abb9d7399cf28528cf
ZhangYajie-NJUST <85613090+ZhangYajie-NJUST@users.noreply.github.com>
2022-12-02 09:44:57 +0800

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -1,14 +1,15 @@
 import torch
 import numpy as np
 import os
-
+import random
+import math
 user=os.getenv('SLURM_JOB_USER')
 job=os.getenv('SLURM_JOB_ID')
 #tmp='/ssd/'+user+'/'+job+'/fine_grained_dataset'
 #tmp='/home/yajie/文档/fine_grained_dataset'
 #tmp='/mnt/10501001/fine_grained_dataset'
-tmp='/home/data_10501005'
-
+# tmp='/home/data_10501005'
+tmp=r'/home/jinlu/data/luzhengyun/dingxinhao/fine-grained-dataset'
 def one_hot_label(label,num_class):
     label=label.view(-1,1).long().cpu()
     onehot=torch.zeros(label.shape[0],num_class)
@@ -30,3 +31,122 @@ def config_dataset(config):
         config["n_class"]=120
     return config
 
+def hadamard(n, dtype=int):
+    """
+    Construct an Hadamard matrix.
+
+    Constructs an n-by-n Hadamard matrix, using Sylvester's
+    construction. `n` must be a power of 2.
+
+    Parameters
+    ----------
+    n : int
+        The order of the matrix. `n` must be a power of 2.
+    dtype : dtype, optional
+        The data type of the array to be constructed.
+
+    Returns
+    -------
+    H : (n, n) ndarray
+        The Hadamard matrix.
+
+    Notes
+    -----
+    .. versionadded:: 0.8.0
+
+    Examples
+    --------
+    >>> from scipy.linalg import hadamard
+    >>> hadamard(2, dtype=complex)
+    array([[ 1.+0.j,  1.+0.j],
+           [ 1.+0.j, -1.-0.j]])
+    >>> hadamard(4)
+    array([[ 1,  1,  1,  1],
+           [ 1, -1,  1, -1],
+           [ 1,  1, -1, -1],
+           [ 1, -1, -1,  1]])
+
+    """
+
+    # This function is a slightly modified version of the
+    # function contributed by Ivo in ticket #675.
+
+    if n < 1:
+        lg2 = 0
+    else:
+        lg2 = int(math.log(n, 2))
+    if 2 ** lg2 != n:
+        raise ValueError("n must be an positive integer, and n must be "
+                         "a power of 2")
+
+    H = np.array([[1]], dtype=dtype)
+
+    # Sylvester's construction
+    for i in range(0, lg2):
+        H = np.vstack((np.hstack((H, H)), np.hstack((H, -H))))
+    return H
+
+def get_hash_targets(n_class, bit):
+    if 2** int(math.log(bit, 2)) ==bit:
+        H_K = hadamard(bit)
+        H_2K = np.concatenate((H_K, -H_K), 0)
+        hash_targets = torch.from_numpy(H_2K[:n_class]).float()
+
+        if H_2K.shape[0] < n_class:
+            hash_targets.resize_(n_class, bit)
+            for k in range(20):
+                for index in range(H_2K.shape[0], n_class):
+                    ones = torch.ones(bit)
+                    # Bernouli distribution
+                    sa = random.sample(list(range(bit)), bit // 2)
+                    ones[sa] = -1
+                    hash_targets[index] = ones
+                # to find average/min  pairwise distance
+                c = []
+                for i in range(n_class):
+                    for j in range(n_class):
+                        if i < j:
+                            TF = sum(hash_targets[i] != hash_targets[j])
+                            c.append(TF)
+                c = np.array(c)
+
+                # choose min(c) in the range of K/4 to K/3
+                # see in https://github.com/yuanli2333/Hadamard-Matrix-for-hashing/issues/1
+                # but it is hard when bit is  small
+                if c.min() > bit / 4 and c.mean() >= bit / 2:
+                    print(c.min(), c.mean())
+                    break
+        return hash_targets
+    else:
+        len=bit
+        bit=2** (int(math.log(bit, 2))+1)
+        H_K = hadamard(bit)
+        H_2K = np.concatenate((H_K, -H_K), 0)
+        hash_targets = torch.from_numpy(H_2K[:n_class]).float()
+
+        if H_2K.shape[0] < n_class:
+            hash_targets.resize_(n_class, bit)
+            for k in range(20):
+                for index in range(H_2K.shape[0], n_class):
+                    ones = torch.ones(bit)
+                    # Bernouli distribution
+                    sa = random.sample(list(range(bit)), bit // 2)
+                    ones[sa] = -1
+                    hash_targets[index] = ones
+                # to find average/min  pairwise distance
+                c = []
+                for i in range(n_class):
+                    for j in range(n_class):
+                        if i < j:
+                            TF = sum(hash_targets[i] != hash_targets[j])
+                            c.append(TF)
+                c = np.array(c)
+
+                # choose min(c) in the range of K/4 to K/3
+                # see in https://github.com/yuanli2333/Hadamard-Matrix-for-hashing/issues/1
+                # but it is hard when bit is  small
+                if c.min() > bit / 4 and c.mean() >= bit / 2:
+                    print(c.min(), c.mean())
+                    break
+        return hash_targets[:,:len]
+    
