cc582f8a0d09d305c17bd9e62d8fdcfc14e9b8f3
ZhangYajie-NJUST <85613090+ZhangYajie-NJUST@users.noreply.github.com>
2022-12-02 09:47:11 +0800

    Update cub_train.py

--- cub_train.py
+++ cub_train.py
@@ -12,7 +12,9 @@ import time
 import torch.nn.functional as Fc
 import itertools
 import os
-
+import logging
+logging.basicConfig(filename='hash_sRLH_cub.log', format='%(asctime)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
 def get_config():
     config = {
         "info": "[sRLH-CUB]",
@@ -21,10 +23,11 @@ def get_config():
         "dataset": "CUB",
         "epoch": 300,
         "test_map": 15,
+        "alpha" :0.9,
         # "device":torch.device("cpu"),
-        "device": torch.device("cuda"),
+        "device": torch.device("cuda:2"),
         "bit_list": [16,32,48,64],
-        "num_workers": 8,
+        "num_workers": 16,
         "channels": 512,
         "init_lr": 0.001,   #todo should be 0.0001
         "weight_decay": 1e-4,
@@ -33,20 +36,22 @@ def get_config():
     }
     config = config_dataset(config)
     return config
-
 def gram_schmidt(A):
     """Gram-schmidt正交化"""
-    Q = np.zeros_like(A)
+    Q = np.zeros_like(A.T)
     cnt = 0
-    for a in A.T:
+    for a in A:
         u = np.copy(a)
-        for i in range(0, cnt):
-            u -= np.dot(np.dot(Q[:, i].T, a), Q[:, i])  # 减去待求向量在以求向量上的投影
+        if cnt==0:
+            pass
+        else:
+            for i in range(cnt-1, cnt):
+                u -= np.dot(np.dot(Q[:, i].T, a), Q[:, i])  # 减去待求向量在以求向量上的投影
         e = u / (np.linalg.norm(u))  # 归一化
         Q[:, cnt] = e
         cnt += 1
-    Q = np.sign(Q)
-    return Q
+    # Q = np.sign(Q.T)
+    return Q.T
 
 
 def main(config, bit,margin):
@@ -67,7 +72,7 @@ def main(config, bit,margin):
         torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
 
-    seed = random.randint(1, 1000)
+    seed = 100
     seed_torch(seed)
     print(f'seed:{seed}')
     model = MainNet(num_classes=config["n_class"], channels=config["channels"], bit=bit)
@@ -86,7 +91,8 @@ def main(config, bit,margin):
     B = torch.zeros(n_select_train, bit).to(device)
     B_label = torch.ones(n_select_train, config["n_class"]).to(device)
 
-    C= torch.sign(torch.rand(config["n_class"], bit) - 0.5).to(device)
+    # C= torch.sign(torch.rand(config["n_class"], bit) - 0.5).to(device)
+    C = get_hash_targets(config["n_class"], bit).to(device)
     center_label = torch.tensor(np.arange(0, config["n_class"], 1)).float()
     center_label_oh = one_hot_label(center_label, config["n_class"]).to(device)
 
@@ -147,11 +153,12 @@ def main(config, bit,margin):
             total_loss.backward()
             optimizer.step()
 
-        print('epoch:{},bit:{},margin:{},loss:{:.4f}'.format(epoch, bit, margin, loss))
+        logger.info('epoch:{},bit:{},margin:{},loss:{:.4f}'.format(epoch, bit, margin, loss))
         if epoch > 1:
             C = (B_label.t()) @ B
-            C = C / torch.sum(B_label.t(), dim=1, keepdim=True)
-            C = torch.tensor(gram_schmidt(C.cpu().numpy())).to(device)
+            C=Fc.normalize(C / torch.sum(B_label.t(), dim=1, keepdim=True),dim=1)
+            # C=C / torch.sum(B_label.t(), dim=1, keepdim=True)
+            C = ((config["alpha"])*C+(1-config["alpha"])*torch.tensor(gram_schmidt(C.cpu().numpy())).to(device)).sign()
         if (epoch + 1) % 50 == 0:
             model.snapshot(config["dataset"],epoch, bit)
             model.eval()
@@ -181,11 +188,11 @@ def main(config, bit,margin):
                     train_label_oh[index, :] = label_oh.cpu()
             mAP = mean_average_precision(test_code, test_label_oh, train_code, train_label_oh, num_train)
             current_time = time.strftime('%H:%M:%S', time.localtime(time.time()))
-            print('epoch:{},time:{},mAP:{:.4f}'.format(epoch, current_time, mAP))
+            logger.info('epoch:{},time:{},mAP:{:.4f}'.format(epoch, current_time, mAP))
 
 if __name__ == '__main__':
     config = get_config()
-    print(config)
+    logger.info(config)
     for bit in config["bit_list"]:
         for m in config["m"]:
             main(config, bit,m)
